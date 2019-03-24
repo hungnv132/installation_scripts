6e3b527b54a36ec4ec46058d1a6918a9b3b30f6f
hungnv132 <hungnv132@gmail.com>
2019-03-24 11:17:50 +0700

    set git config push default

--- git_shortcut.sh
+++ git_shortcut.sh
@@ -3,5 +3,7 @@ echo "alias gp='git push '" >> ~/.bashrc
 echo "alias gpl='git pull '" >> ~/.bashrc
 echo "alias gcm='git commit -m '" >> ~/.bashrc
 
+# git config
+git config --global push.default current
 
 
