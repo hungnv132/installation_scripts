2d43196400248bdb0a93e7d9728e6485df78b950
hungnv132 <hungnv132@gmail.com>
2019-03-27 13:54:46 +0700

    update git shortcuts

--- git_shortcut.sh
+++ git_shortcut.sh
@@ -1,9 +1,12 @@
+# System alias
 echo "alias gst='git status '" >> ~/.bashrc
 echo "alias gp='git push '" >> ~/.bashrc
 echo "alias gpl='git pull '" >> ~/.bashrc
 echo "alias gcm='git commit -m '" >> ~/.bashrc
 
-# git config
+# git config alias
 git config --global push.default current
+git config --global alias.ck checkout
+
 
 
