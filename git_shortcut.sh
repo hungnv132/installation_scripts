ab68f9e2c731c23399e36395db7a3fb1817a9036
hungnv132 <hungnv132@gmail.com>
2019-03-28 16:16:39 +0700

    update git command shortcuts configuration

--- git_shortcut.sh
+++ git_shortcut.sh
@@ -3,6 +3,9 @@ echo "alias gst='git status '" >> ~/.bashrc
 echo "alias gp='git push '" >> ~/.bashrc
 echo "alias gpl='git pull '" >> ~/.bashrc
 echo "alias gcm='git commit -m '" >> ~/.bashrc
+echo "alias gckd='git checkout develop '" >> ~/.bashrc
+echo "alias gpld='git pull origin develop '" >> ~/.bashrc
+
 
 # git config alias
 git config --global push.default current
